802f2c69d8c9f48ef3f1b861cf0b5697c3bfb308
Muzaffar <mdmuzaffarprof@gmail.com>
2024-03-03 15:17:39 +0530

    templated

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+//onchange function implementation
 function changeoption() {
     var x = document.getElementById("mySelect").value;
     document.getElementById("change-description").innerHTML =  x ;
 }
+
+//onclick function implementation
+
+
+
+//onmouseover function implementation
+
+
+
+//onmouseout function implementation
+
+
+
+//onkeydown function implementation
